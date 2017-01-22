62555b133908fcd1130f105a71dfc98ad7f71592
zideajang <zidea2015@163.com>
2017-01-22 12:59:09 +0800

    循环与条件

--- lessonOne/index.js
+++ lessonOne/index.js
@@ -4,12 +4,24 @@ var app = new Vue({
         username:"",
         colorstyle:"",
         attackcount:0,
-        magic:0
+        magic:0,
+        show:true,
+        employeelist:[
+            { name:'Matthew', age: 28},
+            { name:'Tina', age: 26},
+            { name:'Jerry', age: 30}
+        ]
     },
     methods:{
         attack(){
             this.attackcount++;
-            this.magic = -this.attackcount * 2;
+            // this.magic = -this.attackcount * 2;
+        }
+    },
+    computed:{
+        magic(){
+            return -( this.attackcount * 0.5 );
         }
     }
+
 })
