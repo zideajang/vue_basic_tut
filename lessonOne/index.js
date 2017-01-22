e63586be6f08cc8456b67a7cb157d042803a13e7
zideajang <zidea2015@163.com>
2017-01-22 18:34:48 +0800

    创建项目小事例

--- lessonOne/index.js
+++ lessonOne/index.js
@@ -5,25 +5,41 @@ Vue.filter('depart',function(value){
 var app = new Vue({
     el:'#app',
     data:{
+        message:"",
         username:"",
         colorstyle:"",
         attackcount:0,
-        magic:0,
+        sMessage:"",
         show:true,
         employeelist:[
             { name:'Matthew', age: 28, salary:3000},
             { name:'Tina', age: 26, salary:2800},
             { name:'Jerry', age: 30, salary:3200}
+        ],
+        projectName:"",
+        projects:[
+            {
+                name:"SAP"
+            }
         ]
     },
     methods:{
         attack(){
             this.attackcount++;
             // this.magic = -this.attackcount * 2;
+        },
+        sendMsg:function(){
+            this.sMessage = this.username + " 发送: " + this.message;       
+            console.log("send a message");
+        },
+        createProject(){
+            this.projects.push({
+                name:this.projectName
+            })
         }
     },
     computed:{
-        magic(){
+        magic:function(){
             return -( this.attackcount * 0.5 );
         }
     },
